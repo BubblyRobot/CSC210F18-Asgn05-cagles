cf79adac9d1842af2685fd173bce9dbc8ea806c0
BubblyRobot <susyjustice@gmail.com>
2018-10-17 21:19:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/fragmentexample/MainActivity.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.fragmentexample;
+
+
+import android.app.FragmentTransaction;
+import android.app.FragmentManager;
+import android.app.Fragment;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private Boolean isFragmentDisplayed = false;
+    static final String STATE_FRAGMENT = "state_of_fragment";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton = findViewById(R.id.open_button);
+
+        // Set the click listerner for the button
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isFragmentDisplayed) {
+                    displayFragment();
+                } else {
+                    closeFragment();
+                }
+            }
+        });
+
+        if (savedInstanceState !=null){
+            isFragmentDisplayed = savedInstanceState.getBoolean(STATE_FRAGMENT);
+
+            if (isFragmentDisplayed) {
+                // if the fragment is displayed, change button to close.
+                mButton.setText(R.string.close);
+            }
+        }
+
+    }
+    public void displayFragment() {
+        SimpleFragment simpleFragment = SimpleFragment.newInstance();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+
+        // Add the SimpleFragment
+        fragmentTransaction.add(R.id.fragment_container, simpleFragment).addToBackStack(null).commit();
+
+        //update the button text.
+        mButton.setText(R.string.close);
+        // Set boolean flag to indicate fragment is open.
+
+        isFragmentDisplayed = true;
+
+
+    }
+
+    public void closeFragment(){
+        // Get the Fragment Manager.
+        FragmentManager fragmentManager = getFragmentManager();
+        //Check to see if the fragment is already showing.
+        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager.findFragmentById(R.id.fragment_container);
+        if (simpleFragment != null){
+            // Create and commit the transaction to remove the fragment.
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(simpleFragment).commit();
+        }
+
+        //Update the button text.
+        mButton.setText(R.string.open);
+        // Set boolean flag to indicate fragment is closed.
+        isFragmentDisplayed = false;
+    }
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // save the state of the fragment (true=open, false=closed)
+        savedInstanceState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
+        super.onSaveInstanceState(savedInstanceState);
+
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+
+    }
+}
